b72a30524a84165795ee9e37617336f662f5291e
Connor J. Myers <myers.connor12@gmail.com>
2016-10-13 12:05:29 -0700

    Create RedSmurf.java

---
+++ RedSmurf.java
@@ -0,0 +1,21 @@
+public class RedSmurf extends Smurf
+{
+    public String myPattern;
+    
+    public RedSmurf()
+    {
+        super();
+    }
+
+    public RedSmurf(String name)
+    {
+        super();
+        myName = name;
+    }
+    
+     /*public String makePattern()
+     {
+         String pattern = "";
+         
+        
+*/}
